455cdc6c6710f91a70f76c5ddc59c24aa5e83cba
jachaudh <156561167+jachaudh@users.noreply.github.com>
2025-01-21 10:51:36 -0500

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statements to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print("Simple is better than complex.")
-print("Errors should never pass silently.")
--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 # This is good programming advice
 print("Beautiful is better than ugly")
+print("IF The implementation is easy to explain, it may be a good idea.")
+print("Readapbility counts.")
